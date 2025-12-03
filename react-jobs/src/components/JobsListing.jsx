44472f1c8eaaa5623f46a6e4b2b927cf35a0e268
Joker <chumalungchamling@gmail.com>
2025-12-03 23:44:39 +0545

    second commit

--- react-jobs/src/components/JobsListing.jsx
+++ react-jobs/src/components/JobsListing.jsx
@@ -1,9 +1,26 @@
 import React from "react";
-import jobs from "../jobs.json";
+import { useState, useEffect } from 'react';
 import Jobs from "./Jobs";
 
-const JobsListing = ({isHome = false}) => {
-    const jobListings = isHome ? jobs.slice(0, 3): jobs;
+const JobsListing = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+  }
+  fetchJobs();
+  }, []);
+
   return (
     <>
       {/* Browse Jobs */}
@@ -14,9 +31,9 @@ const JobsListing = ({isHome = false}) => {
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {jobListings.map((job) => (
+            {jobs.map((job) => (
               //  {/* Job Listing*/}
-             <Jobs key={job.id} job={job}/>
+              <Jobs key={job.id} job={job} />
             ))}
           </div>
         </div>
